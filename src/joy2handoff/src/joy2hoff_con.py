934e83cab380f1c8c127dc1809bb28ecb50339b6
trcmohitmandokhot <mandokhotm@trcpg.com>
2021-02-09 03:28:30 +0000

    Added dbw status check functionality V0.0.2

--- src/joy2handoff/src/joy2hoff_con.py
+++ src/joy2handoff/src/joy2hoff_con.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # Package converts standard ROS joy messages into structure needed by handoff.py node
-# Uses custom message structure
-# Version 0.0.1
-# Date 02/07/2021
+# Uses custom message structure. Adds dbw_enabled check functionality.
+# Version 0.0.2
+# Date 02/08/2021
 # Anmol Sidhu, Mohit Mandokhot
 
 import rospy
@@ -13,7 +13,7 @@ from sensor_msgs.msg import Joy
 def joy2hoff_con():
 	
 	""" Pseudo-code
-	# 1. Listen to messages sent by ROS standard joy_node.py
+	# 1. Listen to messages sent by ROS standard joy_node.py. Listen to status of dbw.
 	# 2. Understand which joystick is selected
 	# 3. Find a map table for joystick used. This will be used to relate button press to actions
 	# 4. Convert button press and axes inputs to JoyCont and JoyInst msg outputs
@@ -21,30 +21,40 @@ def joy2hoff_con():
 	
 	rospy.init_node('joy2hoff_con', anonymous=True)
 
+	dbw_enabled = Bool()
+
+	def rec_dbw_enabled(data):
+		if data:
+			print("1. Platform DBW Enabled")
+		else:
+			print("2. Platform DBW Disabled")
+		dbw_enabled = data
+
 	# Accept data published by ROS joy_node 
 	def recd_rosjoyval(data):
 		recd_msgbuttons = data.buttons	#int32[]
 		recd_msgaxes = data.axes	#float32[]
 		# Header data not used
 		#print('# of Buttons in DS: ' str(len(recd_msgbuttons)))
-		publish_discrete(recd_msgbuttons)
+		publish_discrete(recd_msgbuttons, dbw_enabled)
 		#print('# of Axes in DS: ' str(len(recd_msgaxes)))
 		publish_cont(recd_msgaxes)
 
 	# Read Joystick Buttons. Determine content. Publish JoyInst message.
+	# 
 	# Output four modes - enable, disable, manual, automated
 	# Button Maps - buttons[7], buttons[6], buttons[2], buttons[0]. See js_image
-	def publish_discrete(data):
+	def publish_discrete(data, dbw_state):
 		# Recieve data in int32[] list format. Confirm length of list
 		len_msg = len(data)
 		#print(len_msg)
 		sent_msgbuttons = JoyInst()		
 		#If pressed (same as 'handoff' enable), set enable - (No status check)
-		if data[7] == 1:
+		if data[7] == 1 and (not dbw_state):
 		    sent_msgbuttons.enable = True
 		    pub_joy_inst.publish(sent_msgbuttons)
 		#If pressed, set disable (same as 'handoff' disable)
-		if data[6] == 1:
+		if data[6] == 1 and (dbw_state):
 		    sent_msgbuttons.disable = True
 		    pub_joy_inst.publish(sent_msgbuttons)
 		#If pressed, set manual mode (same as 'handoff' Open Loop Mode)
@@ -93,9 +103,14 @@ def joy2hoff_con():
 			sent_msgaxes.aft = 0
 			pub_joy_cont.publish(sent_msgaxes)
 
+
+	# Listen to DBW Enabled from Vehicle Platform (Car - DBW, Sim - VehSim)
+        # dbw_enabled = Bool()
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, rec_dbw_enabled)
 	# Define a subscriber to listen to joy_node on "Joy" topic
 	ros_joyval = Joy()  
         rospy.Subscriber('/joy', Joy, recd_rosjoyval)
+
 
         """ PUBLISHERS """
         # Publish JoyCont and JoyInst messages downstream to handoff
